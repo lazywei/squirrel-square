d525cb73c5303924a4046a56e11c9b9312f114f8
Chih-Wei Chang <bert.cwchang@gmail.com>
2016-01-28 14:46:27 +0800

    Refactor renderer/main.js

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,18 +4,16 @@ import { makeDOMDriver, div, button, p, h1 } from '@cycle/dom'
 import electron from 'electron'
 
 function ipcDriver(request$) {
-    request$.subscribe(req => {
-        electron.ipcRenderer.send(req.msg, req.payload)
-    })
+    request$.subscribe(req => electron.ipcRenderer.send(req.msg, req.payload))
 
-    const ipc$ = Rx.Observable.create((observer) => {
+    const ipc$ = Rx.Observable.create(observer => {
         Rx.Observable
             .fromEvent(electron.ipcRenderer,
                        'ipc-driver-finished',
                        (event, data) => data)
             .subscribe(data => observer.onNext(data))
 
-        return () => { console.log('disposed'); };
+        return () => { console.log('disposed') }
     })
     return ipc$;
 }
@@ -28,20 +26,25 @@ function ipcDriver(request$) {
 // read effects are coming from sources
 
 function main(sources) {
-    const clickEv$ = sources.DOM.select('button').events('click')
+    const clickEv$ = sources.DOM.select('button')
+          .events('click')
+          .startWith({/* Fake a click event for initial loading*/})
     const request$ = clickEv$.map(() => {
-        return { msg: 'load-data', payload: { filename: 'default.custom.yaml' } }
+        return {
+            msg: 'load-data',
+            payload: { filename: 'default.custom.yaml' }
+        }
     })
-
-    const data$ = sources.IPC.startWith(null);
+    const data$ = sources.IPC;
 
     return {
         DOM: data$.map(data =>
                  div([
                      h1('Menu'),
-                     p(`Page Size: ${data === null ?
-                                     '' : data.patch.menu.pageSize}`),
-                     button("Load Data"),
+                     p(`Page Size: ${data.patch.menu.pageSize}`),
+                     h1('Schema List'),
+                     data.patch.schemaList.map(({schema}) => p(schema)),
+                     button("Reload Data"),
                  ])
                 ),
         IPC: request$,
